198da08b4667da01f6267274a7e769b23f411d7f
AlexBezkopylnyi <sanok.bes@gmail.com>
2019-04-30 18:43:15 +0300

    Initial commit

---
+++ MyCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Alexandr on 4/30/19.
+//  Copyright © 2019 Alex.Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
